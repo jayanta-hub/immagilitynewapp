b7914d5fd3642d40fb1e05b6973d25e5d3260e9d
jayanta <jayanta.garu@arithaconsulting.com>
2023-04-07 14:41:04 +0530

    sing in and sing out functionality added

--- src/Infrastructure/navigation/DrawerNavigator.js
+++ src/Infrastructure/navigation/DrawerNavigator.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View} from 'react-native';
+import {Alert, Text, View} from 'react-native';
 import {
   createDrawerNavigator,
   DrawerItemList,
@@ -8,11 +8,69 @@ import {
 import BottomNavigator from './BottomNavigator';
 import Profile from '../../presentation/components/Profile/Profile';
 import {scale} from '../utils/screenUtility';
+import {useNavigation} from '@react-navigation/native';
+import {AuthContext} from '../utils/context';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
 const CustomDrawer = props => {
+  const navigation = useNavigation();
+
+  const {signOut} = React.useContext(AuthContext);
+  const showAlert = () => {
+    Alert.alert(
+      'ImmigrationHub',
+      'Would you like to Logout',
+      [
+        {
+          text: 'No',
+          onPress: () => console.log('close'),
+          style: 'cancel',
+        },
+        {
+          text: 'Yes',
+          onPress: () => {
+            navigation.goBack(), signOut();
+          },
+        },
+      ],
+      {cancelable: false},
+    );
+  };
   return (
     <View style={{flex: 1}}>
       <DrawerContentScrollView style={{height: '100%', position: 'relative'}}>
         <DrawerItemList {...props} />
+        <View
+          style={{
+            marginHorizontal: scale(12),
+            borderWidth: 0.8,
+            borderStyle: 'dashed',
+            borderColor: '#C3C3C3',
+            marginBottom: scale(20),
+            marginTop: scale(20),
+          }}
+        />
+        <TouchableOpacity
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            marginLeft: scale(9),
+            flexDirection: 'row',
+            paddingVertical: scale(10),
+          }}
+          onPress={showAlert}>
+          <MaterialIcons name="power-settings-new" size={22} color="#00A8DB" />
+          <Text
+            style={{
+              fontFamily: 'SourceSansPro-Regular',
+              fontSize: scale(14),
+              color: '#4D4F5C',
+              marginLeft: scale(5),
+            }}>
+            Logout
+          </Text>
+        </TouchableOpacity>
       </DrawerContentScrollView>
     </View>
   );
